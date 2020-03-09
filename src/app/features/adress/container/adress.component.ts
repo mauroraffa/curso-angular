c40b4db061b9fe184dc424038c24846d4ada120b
unknown <you@example.com>
2020-03-09 18:34:27 -0300

    feat: unsuscribe in load states

--- src/app/features/adress/container/adress.component.ts
+++ src/app/features/adress/container/adress.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterContentInit, OnDestroy } from '@angular/core';
 import { AdressService } from '../services/adress.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-adress',
     templateUrl: './adress.component.html',
     styleUrls: ['./adress.component.scss']
 })
-export class AdressComponent implements OnInit, AfterViewInit {
+export class AdressComponent implements OnInit, AfterViewInit, OnDestroy {
 
-    states: [];
+    states: [] = [];
+
+    loadStatesUnsuscribe: Subscription;
 
     constructor(
         private domicilioService: AdressService
@@ -19,17 +22,26 @@ export class AdressComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
+
     }
 
     loadSelect() {
-        this.domicilioService.loadStates().subscribe((response) => {
+        this.loadStatesUnsuscribe = this.domicilioService.loadStates().subscribe((response) => {
             if (response) {
                 this.states = response.provincias;
+                console.log('Adentro de la suscripcion', this.states);
             }
         });
+        console.log('Afuera de la suscripcion', this.states);
     }
 
     saveAdress() {
         console.log('guardando domicilio');
+    }
+
+    ngOnDestroy() {
+        if (this.loadStatesUnsuscribe) {
+            this.loadStatesUnsuscribe.unsubscribe();
+        }
     }
 }
