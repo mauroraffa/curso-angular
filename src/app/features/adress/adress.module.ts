b2cc0beb47919da56b800433da6ca7d16a53e6f5
unknown <you@example.com>
2020-03-11 18:02:40 -0300

    feat: redux

--- src/app/features/adress/adress.module.ts
+++ src/app/features/adress/adress.module.ts
@@ -4,10 +4,17 @@ import { AdressService } from './services/adress.service';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from 'src/app/shared/component/components.module';
 
+import * as fromAdressReducer from './store/adress.reducer';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { AdressEffects } from './store/adress.effects';
+
 @NgModule({
     imports: [
         CommonModule,
-        ComponentsModule
+        ComponentsModule,
+        StoreModule.forFeature('adress', fromAdressReducer.reducer),
+        EffectsModule.forFeature([AdressEffects])
     ],
     declarations: [
         AdressComponent
--- src/app/features/adress/container/adress.component.ts
+++ src/app/features/adress/container/adress.component.ts
@@ -2,37 +2,43 @@ import { Component, OnInit, AfterViewInit, AfterContentInit, OnDestroy } from '@
 import { AdressService } from '../services/adress.service';
 import { Subscription } from 'rxjs';
 
+import * as fromActionsAdress from '../store/adress.actions';
+import * as fromSelectorAdress from '../store/adress.selectors';
+import { Store, select } from '@ngrx/store';
+import { IAdress } from '../store/adress.reducer';
+import { filter } from 'rxjs/operators';
+
 @Component({
     selector: 'app-adress',
     templateUrl: './adress.component.html',
     styleUrls: ['./adress.component.scss']
 })
-export class AdressComponent implements OnInit, AfterViewInit, OnDestroy {
+export class AdressComponent implements OnInit, OnDestroy {
 
     states: [] = [];
 
     loadStatesUnsuscribe: Subscription;
 
     constructor(
-        private domicilioService: AdressService
+        private store: Store<IAdress>
     ) { }
 
     ngOnInit() {
+        this.loadStatesUnsuscribe = this.store
+            .pipe(select(fromSelectorAdress.selectData))
+            .pipe(filter(val => !!val))
+            .subscribe((data: any) => {
+                console.log(data);
+            });
         this.loadSelect();
     }
 
-    ngAfterViewInit() {
-
-    }
-
     loadSelect() {
-        this.loadStatesUnsuscribe = this.domicilioService.loadStates().subscribe((response) => {
-            if (response) {
-                this.states = response.provincias;
-                console.log('Adentro de la suscripcion', this.states);
-            }
-        });
-        console.log('Afuera de la suscripcion', this.states);
+        // this.loadStatesUnsuscribe = this.domicilioService.loadStates().subscribe((response) => {
+        //     if (response) {
+        //         this.states = response.provincias;
+        //     }
+        // });
     }
 
     saveAdress() {
